969d50fe6ae78ccb6c05443daf94caa96b55af0a
chwoozy <choonweiw@gmail.com>
2022-05-03 13:40:48 -0400

    Deploy React app to GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd53960bd40312c0b50618bdb0f8afd8.js"
+  "/site/precache-manifest.f3d11650d8c19de0d1a50c638181aaf9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/site/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
