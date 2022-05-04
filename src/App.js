c49f6aec5fbecc64bf4b7fdd4296470a4710195d
chwoozy <choonweiw@gmail.com>
2022-05-04 14:21:08 -0400

    AnikiLabs Launch 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Logo from "./components/Logo";
 
 function App() {
+  document.title = "Aniki Labs // 2022";
   return (
     <div className="container">
       <Logo />
-      <p className="elevated">Aniki Labs // 2022</p>
+      <p className="elevated flex-left">Aniki Labs // 2022</p>
     </div>
   );
 }
